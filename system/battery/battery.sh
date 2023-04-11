c18fab7bac0f04e82e5815f6f0f39d86ccf88aa4
carnivuth <matti200042@gmail.com>
2023-04-11 23:19:20 +0200

    added low battery notification

---
+++ system/battery/battery.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+
+# setting parameters
+NOTIFY=TRUE
+LIMIT=93
+while :
+do
+    PERCENTAGE=$(upower -i $(upower -e |grep BAT1) |grep percentage |cut -d':' -f2 |cut -d '%' -f1 |xargs)
+    STATE=$(upower -i $(upower -e |grep BAT1) |grep state |cut -d':' -f2 |xargs)
+    if [ "$STATE" == "charging" ]; then 
+        NOTIFY=TRUE
+
+    else
+        if [[ "$PERCENTAGE" -lt $LIMIT ]]; then 
+            if [ "$NOTIFY" == "TRUE" ]; then
+            notify-send -a "Low Battery" -u critical "low battery" "the system battery has reach the $LIMIT%"
+            NOTIFY=FALSE
+            fi
+        fi
+    fi 
+    sleep 10
+done
