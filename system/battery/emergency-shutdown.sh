54e5d96bbc967b1c2fe4614d174bb94494badcd7
carnivuth <matti200042@gmail.com>
2023-04-16 15:13:56 +0200

    added emergency shut down script

---
+++ system/battery/emergency-shutdown.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# setting parameters
+NOTIFY=TRUE
+LIMIT=3
+notify-send -a "Low Battery" -u critical "low battery" "the system will be shut down"
+
+while :
+do
+    PERCENTAGE=$(upower -i $(upower -e |grep BAT1) |grep percentage |cut -d':' -f2 |cut -d '%' -f1 |xargs)
+    STATE=$(upower -i $(upower -e |grep BAT1) |grep state |cut -d':' -f2 |xargs)
+    if [ "$STATE" == "charging" ]; then 
+        NOTIFY=TRUE
+
+    else
+        if [[ "$PERCENTAGE" -lt $LIMIT ]]; then 
+            if [ "$NOTIFY" == "TRUE" ]; then
+            notify-send -a "Low Battery" -u critical "low battery" "the system will be shut down"
+            sleep 2 
+            systemctl poweroff
+            NOTIFY=FALSE
+            fi
+        fi
+    fi 
+    sleep 10
+done
