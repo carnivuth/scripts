2dbca85ae0b6afb6bed9ce0f97623e9619751e0e
carnivuth <matti200042@gmail.com>
2023-04-19 16:51:40 +0200

    fixed notification bug

--- system/notifications/alert.sh
+++ system/notifications/alert.sh
@@ -2,7 +2,7 @@
 
 echo "$@" >> /home/matteo/scripts/system/notifications/logs
 if [ "$1" != "Spotify" ]; then
-	if [ "$1"!= "networkmanager-dmenu" ]; then
+	if [ "$1" != "networkmanager-dmenu" ]; then
     paplay /home/matteo/scripts/system/notifications/notification.wav
 fi
 fi
