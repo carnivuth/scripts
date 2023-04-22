93c86e35d98ca5ff0c0af97f02cc656d80899201
carnivuth <matti200042@gmail.com>
2023-04-22 17:41:22 +0200

    added generic path

--- borg/home_backup.sh
+++ borg/home_backup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-borg create --info /storage/borg::Home-"$(date +%c)" /home/matteo 2>> /home/matteo/scripts/borg/logs >> /home/matteo/scripts/borg/logs
+borg create --info /storage/borg::Home-"$(date +%c)" /home/matteo 2>> $HOME/scripts/borg/logs >> $HOME/scripts/borg/logs
