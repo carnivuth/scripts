c6997e5cfb64ab3c59bab05a64bca7059d315e1f
carnivuth <matti200042@gmail.com>
2023-04-22 15:31:26 +0200

    refactor logseq folder

--- logseq-stuff/cloneAppunti.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir ~/appunti
-echo "cloning repositories"
-echo "----------------------"
-cat repo_appunti |while read REPO ; do
-    git clone "$REPO" ~/appunti/$(echo $REPO |rev| cut -d/ -f1 |rev |cut -d. -f1)
-done
