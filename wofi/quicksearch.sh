3a9885edcf57140d312017f70d969e89809bcb2e
carnivuth <matti200042@gmail.com>
2023-04-22 15:14:10 +0200

    added comments

--- wofi/quicksearch.sh
+++ wofi/quicksearch.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-#project folders
-OPTIONS=( "Shutdown" "reboot" "sleep" "exit" "lock" )
-#wofi utilities
 
 
-# Pass variables to rofi dmenu
+# wofi cmd
 run_wofi() {
 
 	 wofi --show dmenu --prompt='firefox search' --heigh="5%"
 }
-# show rofi with folders
+# main
 chosen="$(run_wofi)"
 if [ "$chosen" != "" ]; then 
 	firefox --new-window --search "$chosen"
