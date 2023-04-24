b43ce3e4dd125657d346cd2ca19e94cd562acf89
carnivuth <matti200042@gmail.com>
2023-04-24 16:01:58 +0200

    add filebrowser script

---
+++ rofi/filebrowser.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+dir="$HOME/.config/rofi/filebrowser/"
+theme='snorlax-center'
+if [ "$#" -eq 1 ]; then 
+    theme="$1"
+fi
+if [ -f "${dir}/${theme}.rasi"  ]; then
+
+## Run
+    rofi -show file-browser-extended \
+         -file-browser-dir "$HOME" -file-browser-depth 100 \
+         -theme ${dir}/${theme}.rasi \
+         -p "Files" 
+
+else
+## Run without theme
+    rofi -show file-browser-extended \
+         -file-browser-dir "$HOME" -file-browser-depth 100 \
+         -p "Files"
+fi
