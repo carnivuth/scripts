6791018c50aa8452c0d4b0a68256213fe84fb77c
carnivuth <matti200042@gmail.com>
2023-04-28 12:59:18 +0200

    removed arguments

--- rofi/filebrowser.sh
+++ rofi/filebrowser.sh
@@ -9,13 +9,13 @@ if [ -f "${dir}/${theme}.rasi"  ]; then
 
 ## Run
     rofi -show file-browser-extended \
-         -file-browser-dir "$HOME" -file-browser-depth 100 \
+         -file-browser-dir "$HOME"   \
          -theme ${dir}/${theme}.rasi \
          -p "Files" 
 
 else
 ## Run without theme
     rofi -show file-browser-extended \
-         -file-browser-dir "$HOME" -file-browser-depth 100 \
+         -file-browser-dir "$HOME"  \
          -p "Files"
 fi
