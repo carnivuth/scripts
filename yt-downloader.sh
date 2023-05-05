8385066b5d51882f16cec36bc4c4a948cd39ae4b
carnivuth <matti200042@gmail.com>
2023-05-05 17:33:21 +0200

    removed progress.txt

--- yt-downloader.sh
+++ yt-downloader.sh
@@ -3,3 +3,4 @@ if [[ "$1" == "--help" ]]; then
     echo 'parameters dest-folder URL'
 fi
 yt-dlp --ignore-errors --continue --no-overwrites --download-archive progress.txt -x -P "$HOME/Musica/$1" -f bestaudio  "$2"
+rm progress.txt
