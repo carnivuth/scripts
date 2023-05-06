f2ee48ad8d8595c94f827d6c612ed5c1c8dc7706
carnivuth <matti200042@gmail.com>
2023-05-06 15:46:47 +0200

    refactor yt downloader scripts

--- utilities/yt-downloader.sh
+++ utilities/yt-downloader.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 if [[ "$1" == "--help" ]]; then
     echo 'parameters dest-folder URL'
+    exit 0
 fi
+if [[ "$#" -ne 2 ]]; then   
+    echo "wrong parameters"
+    exit 1
+fi 
 yt-dlp --ignore-errors --continue --no-overwrites --download-archive progress.txt -x -P "$HOME/Musica/$1" -f bestaudio  "$2"
 rm progress.txt
