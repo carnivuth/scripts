711086b77d7c003b0f223a6293104b85e861bd8a
carnivuth <matti200042@gmail.com>
2023-05-19 19:00:00 +0200

    added scan and connect function

--- rofi/networkmanager/networkmenu.sh
+++ rofi/networkmanager/networkmenu.sh
@@ -1,10 +1,48 @@
 #!/bin/bash
+
 # set rofi theme 
-dir="$HOME/.config/rofi/filebrowser/"
+dir="$HOME/.config/rofi/network-menu/"
 theme='snorlax-line'
 if [ "$#" -eq 1 ]; then 
     theme="$1"
 fi
-# get wirless connections 
+
+# prompt
+prompt='Networks'
+
+# Rofi CMD
+rofi_cmd() {
+	if [ -f "${dir}/${theme}.rasi"  ]; then
+		rofi -dmenu \
+        -p "${prompt}" \
+			-theme ${dir}/${theme}.rasi
+	else
+		rofi -dmenu \
+        -p "${prompt}"
+	
+	fi
+}
+
 # print rofi menu to user with possible options
-# get option and run nmcli command
+print_networks() {
+	 echo -e "$(nmcli -f ssid device wifi list | grep -v SSID)" | rofi_cmd
+}
+
+# main
+selected=$(print_networks| xargs)
+
+# if no network was selected exit
+if [ "$selected" == '' ]; then exit 0; fi
+
+# connect to network
+if [ "$(nmcli connection | grep "$selected")" != "" ]; then
+
+    nmcli device wifi connect "$selected"
+
+elif [ "$(nmcli -f ssid device wifi list -rescan no | grep "$selected" )" != '']; then 
+    
+    #ask for network password
+    password=$(rofi_cmd)
+    nmcli device wifi connect "$selected" password "$password"
+
+fi
