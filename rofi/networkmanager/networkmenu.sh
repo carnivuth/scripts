684611d19d41e8e873c881be2b2134262682a008
carnivuth <matti200042@gmail.com>
2023-05-19 19:24:45 +0200

    added ask for password function

--- rofi/networkmanager/networkmenu.sh
+++ rofi/networkmanager/networkmenu.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
 # set rofi theme 
-dir="$HOME/.config/rofi/network-menu/"
+dir="$HOME/.config/rofi/networkmenu/"
 theme='snorlax-line'
 if [ "$#" -eq 1 ]; then 
     theme="$1"
 fi
 
-# prompt
+# prompts
 prompt='Networks'
+prompt_pwd='Password'
 
 # Rofi CMD
 rofi_cmd() {
 	if [ -f "${dir}/${theme}.rasi"  ]; then
 		rofi -dmenu \
-        -p "${prompt}" \
+        -p "$1" \
 			-theme ${dir}/${theme}.rasi
 	else
 		rofi -dmenu \
-        -p "${prompt}"
+        -p "$1"
 	
 	fi
 }
 
 # print rofi menu to user with possible options
 print_networks() {
-	 echo -e "$(nmcli -f ssid device wifi list | grep -v SSID)" | rofi_cmd
+	 echo -e "$(nmcli -f ssid device wifi list | grep -v SSID)" | rofi_cmd ${prompt}
+}
+ask_password(){
+    rofi_cmd ${prompt_pwd}
 }
 
 # main
@@ -39,10 +43,13 @@ if [ "$(nmcli connection | grep "$selected")" != "" ]; then
 
     nmcli device wifi connect "$selected"
 
-elif [ "$(nmcli -f ssid device wifi list -rescan no | grep "$selected" )" != '']; then 
+elif [ "$(nmcli -f ssid device wifi list -rescan no | grep "$selected" )" != '' ]; then 
     
     #ask for network password
-    password=$(rofi_cmd)
+    password=$(ask_password)
+    #check for empty password
+    if [ "$password" == '' ]; then exit 0; fi
+
     nmcli device wifi connect "$selected" password "$password"
 
 fi
