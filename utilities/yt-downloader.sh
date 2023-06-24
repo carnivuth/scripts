af6b33198867fb92f71c90d9d787c93223bd5465
carnivuth <matti200042@gmail.com>
2023-06-24 09:27:55 +0200

    removed Music folder

--- utilities/yt-downloader.sh
+++ utilities/yt-downloader.sh
@@ -7,5 +7,5 @@ if [[ "$#" -ne 2 ]]; then
     echo "wrong parameters"
     exit 1
 fi
-yt-dlp --ignore-errors --continue --no-overwrites --download-archive progress.txt -x -P "$HOME/Musica/$1" -f bestaudio "$2"
+yt-dlp --ignore-errors --continue --no-overwrites --download-archive progress.txt -x -P "$1" -f bestaudio "$2"
 rm progress.txt
