15d97716fbc412ca5de2135037ba34000b37a2e5
carnivuth <matti200042@gmail.com>
2023-09-12 12:24:57 +0200

    added logs and parameters

--- rofi/musicplayer/musicplayer.sh
+++ rofi/musicplayer/musicplayer.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+ source "$HOME/scripts/settings.sh"
+
 # set rofi theme
 dir="$HOME/.config/rofi/musicplayer"
 theme='snorlax-line'
@@ -32,35 +34,35 @@ chosen="$(print_playlists)"
 
 #kill vlc command
 if [[ "$chosen" == "stop" ]];then
-vlc="$(cat $HOME/scripts/rofi/musicplayer/log)"
+vlc="$(cat $SCRIPTS_RUN_FOLDER/pid.vlc)"
     if [ "$vlc" != '' ]; then
         kill $vlc
-        rm $HOME/scripts/rofi/musicplayer/log
+        rm $SCRIPTS_RUN_FOLDER/pid.vlc
     fi
-    glava="$(cat $HOME/scripts/rofi/musicplayer/log-glava)"
+    glava="$(cat $SCRIPTS_RUN_FOLDER/pid.glava)"
     if [ "$glava" != '' ]; then
         kill $glava
-        rm $HOME/scripts/rofi/musicplayer/log-glava
+        rm $SCRIPTS_RUN_FOLDER/pid.glava
     fi
 
 fi
 
 #run playlist
 if [[ -d "$folder/$chosen" &&  "$chosen" != '' ]]; then
-    vlc="$(cat $HOME/scripts/rofi/musicplayer/log)"
+    vlc="$(cat $SCRIPTS_RUN_FOLDER/pid.vlc)"
     if [ "$vlc" != '' ]; then
         kill $vlc
         sleep 1
     fi
-    glava="$(cat $HOME/scripts/rofi/musicplayer/log-glava)"
+    glava="$(cat $SCRIPTS_RUN_FOLDER/pid.glava)"
     if [ "$glava" != '' ]; then
         kill $glava
         sleep 1
     fi
-    vlc --qt-start-minimized "$folder/$chosen" &
-    echo $! > $HOME/scripts/rofi/musicplayer/log
-    glava --desktop &
-    echo $! > $HOME/scripts/rofi/musicplayer/log-glava
+    vlc --qt-start-minimized "$folder/$chosen" 2 >> $SCRIPTS_LOGS_FOLDER/musicplayer.vlc.log >> $SCRIPTS_LOGS_FOLDER/musicplayer.vlc.log &
+    echo $! > $SCRIPTS_RUN_FOLDER/pid.vlc
+    glava --desktop 2 >> $SCRIPTS_LOGS_FOLDER/musicplayer.glava.log >> $SCRIPTS_LOGS_FOLDER/musicplayer.glava.log &
+    echo $! > $SCRIPTS_RUN_FOLDER/pid.glava
     notify-send -a "Music player" -u "normal" "$chosen" "playing $chosen"
 
 fi
