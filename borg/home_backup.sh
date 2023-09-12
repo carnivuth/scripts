7b51e7448f44f200dc010781602ce2027bb33fa1
carnivuth <matti200042@gmail.com>
2023-09-12 11:24:00 +0200

    added borg  passphrase

--- borg/home_backup.sh
+++ borg/home_backup.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-borg create --info /storage/borg::Home-"$(date +%c)" /home/matteo 2>> /home/matteo/scripts/borg/logs >> /home/matteo/scripts/borg/logs
+source $HOME/scripts/settings.sh
+if [ -f "$SCRIPTS_HOME_FOLDER/borg/target.sh" ]; then
+    source "$SCRIPTS_HOME_FOLDER/rofi/target.sh"
+else
+    TARGET="$HOME"
+fi
+## remember to add record on gnome keyring !! 
+## echo "passphrase"| secret-tool store borg-repository repo-name --label="borg passphrase"
+
+export BORG_PASSCOMMAND="secret-tool lookup borg-repository home"
+borg create --info /storage/borg::Home-"$(date +%c)" $TARGET 2>> $SCRIPTS_LOGS_FOLDER/home_backup.logs >> $SCRIPTS_LOGS_FOLDER/home_backup.logs
