0155ef77f952a8a8d6e8f3349f60b2956abdb872
carnivuth <matti200042@gmail.com>
2023-09-28 23:35:03 +0200

    added global install

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+source "$HOME/scripts/settings.sh"
+
+# install DE
+echo 'install carnivuth-DE?[Y/n]'
+unset answer
+
+read answer
+if [[  "$answer" == 'Y' || "$answer" == '' ]]; then
+source $SCRIPTS_HOME_FOLDER/carnivuth-DE/install.sh
+fi
+
+# install DE
+echo 'install desktop files?[Y/n]'
+read answer
+if [[  "$answer" == 'Y' || "$answer" == '' ]]; then
+source $SCRIPTS_HOME_FOLDER/desktop-files/install.sh
+fi
+
+# install DE
+echo 'install systemd files?[Y/n]'
+read answer
+if [[  "$answer" == 'Y' || "$answer" == '' ]]; then
+source $SCRIPTS_HOME_FOLDER/systemd/install.sh
+fi
+
+# install DE
+echo 'install utils?[Y/n]'
+read answer
+if [[  "$answer" == 'Y' || "$answer" == '' ]]; then
+source $SCRIPTS_HOME_FOLDER/utilities/install.sh
+fi
