5ddbafdce8e756362711e38cd45559e3f119d012
carnivuth <matti200042@gmail.com>
2023-10-03 10:59:19 +0200

    added dunst theme switcher

--- carnivuth-DE/rofi/themeswitcher/postwal.sh
+++ carnivuth-DE/rofi/themeswitcher/postwal.sh
@@ -2,3 +2,4 @@
 
 source "$HOME/scripts/settings.sh"
 source "$SCRIPTS_HOME_FOLDER/carnivuth-DE/polybar/polybar-color-switcher.sh"
+source "$SCRIPTS_HOME_FOLDER/carnivuth-DE/dunst/dunst-color-switcher.sh"
