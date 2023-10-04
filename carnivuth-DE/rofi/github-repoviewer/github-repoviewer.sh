f92cd080dc7b2bf7853a05b40d5d21dc1a09681b
carnivuth <matti200042@gmail.com>
2023-10-04 11:12:26 +0200

    added github repo-viewer

---
+++ carnivuth-DE/rofi/github-repoviewer/github-repoviewer.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+source "$HOME/scripts/settings.sh"
+source "$SCRIPTS_LIBS_FOLDER/rofi_standard.sh"
+source "$SCRIPTS_HOME_FOLDER/carnivuth-DE/rofi/github-repoviewer/account.sh"
+
+rofi_theme_setup $ROFI_CONFIG_FOLDER/github-repoviewer "$1" "ghitub repos"
+
+print_menu() {
+    echo -e "$(curl https://api.github.com/users/$account/repos | jq -r '.[] | .html_url')" | rofi_cmd
+
+}
+
+# main
+chosen="$(print_menu)"
+echo $chosen
+# open selected folder on $1 parameter default code
+if [  "$chosen" != "" ]; then
+    firefox --new-window "$chosen"
+fi
