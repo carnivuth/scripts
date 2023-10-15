ce482573ad5de4658fc39e0b19fd4109a7476e58
carnivuth <matti200042@gmail.com>
2023-10-15 18:16:24 +0200

    fixed path for utilities

--- utilities/install.sh
+++ utilities/install.sh
@@ -4,3 +4,4 @@ source "$HOME/scripts/settings.sh"
 # copy config files
 echo 'installing utils'
 export PATH="$SCRIPTS_HOME_FOLDER/utilities/:$PATH"
+echo "export PATH=$SCRIPTS_HOME_FOLDER/utilities/:$PATH" >> "$HOME/.bashrc"
