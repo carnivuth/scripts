aed6f783985faf321119590a135aa892b6b3a721
carnivuth <matti200042@gmail.com>
2023-10-15 17:08:03 +0200

    added variable for flameshot config path

--- carnivuth-DE/install.sh
+++ carnivuth-DE/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source "$HOME/scripts/settings.sh"
 
-# install packages 
+# install packages
 echo 'installing packages'
 packets='pavucontrol firefox brightnessctl ttf-font-awesome code python-pywal alacritty autorandr python-pywal feh conky flameshot i3-wm picom polybar rofi vlc blueman dunst'
 sudo pacman -S $packets
@@ -9,3 +9,10 @@ sudo pacman -S $packets
 # copy config files
 echo 'coping config files'
 cp -r "$SCRIPTS_HOME_FOLDER/carnivuth-DE/.config/"* "$HOME/.config/"
+
+# setting up flameshot savePath folder
+echo 'setting flameshot savePath folder'
+if [[! -d "$FLAMESHOT_FOLDER" ]]; then
+    mkdir "$FLAMESHOT_FOLDER"
+fi
+echo "savePath=$FLAMESHOT_FOLDER" >>"$HOME/.config/flameshot/flameshot.ini"
