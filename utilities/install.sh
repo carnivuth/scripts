bc9dfd3460bff9eddd5247395f28603e89ea6a5d
carnivuth <matti200042@gmail.com>
2023-10-15 18:31:06 +0200

    fixed PATH variable setup

--- utilities/install.sh
+++ utilities/install.sh
@@ -3,7 +3,7 @@ source "$HOME/scripts/settings.sh"
 
 # copy config files
 echo 'installing utils'
-if [[ "$(echo $PATH | grep "$SCRIPTS_HOME_FOLDER/utilities/")" == "" ]]; then
+if [[ "$(cat $HOME/.bashrc | grep $SCRIPTS_HOME_FOLDER/utilities/)" == "" ]]; then
     echo "export PATH=$SCRIPTS_HOME_FOLDER/utilities/:$PATH" >>"$HOME/.bashrc"
     export PATH="$SCRIPTS_HOME_FOLDER/utilities/:$PATH"
 fi
