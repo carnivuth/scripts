af1bccf736834da9af16e74b97b8894289b49ee2
carnivuth <matti200042@gmail.com>
2023-10-15 18:15:45 +0200

    create folder for desktop files

--- desktop-files/install.sh
+++ desktop-files/install.sh
@@ -3,4 +3,5 @@ source "$HOME/scripts/settings.sh"
 
 # copy config files
 echo 'coping desktop files'
+mkdir -p "$HOME/.local/share/applications/"
 cp -r "$SCRIPTS_HOME_FOLDER/desktop-files/"* "$HOME/.local/share/applications/"
