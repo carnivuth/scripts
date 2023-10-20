c994fe25d576b302ee6268025965b75dc6c82c7e
carnivuth <matti200042@gmail.com>
2023-10-20 20:53:51 +0200

    fixed .obsidian folder bug

--- utilities/create_obsidian_vault.sh
+++ utilities/create_obsidian_vault.sh
@@ -42,6 +42,7 @@ fi
 
 if [[ ! -d "$vault/.obsidian" || "$reset" == '1' ]]; then
     echo "copying obsidian files"
+    mkdir "$vault/.obsidian"
     cp -r $SCRIPTS_HOME_FOLDER/utilities/obsidian-files/* "$vault/.obsidian"
 fi
 
