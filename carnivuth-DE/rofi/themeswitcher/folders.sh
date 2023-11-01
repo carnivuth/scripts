8e5ff2b9bca05a0017513050fed1dbebf504951e
carnivuth <matti200042@gmail.com>
2023-11-01 18:23:34 +0100

    removed installation files

--- carnivuth-DE/rofi/themeswitcher/folders.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ARRAY=("/usr/share/backgrounds" "/usr/share/backgrounds/archlinux" "/usr/share/backgrounds/gnome" "$HOME/Immagini/wallpapers")
--- carnivuth-DE/rofi/vaultlauncher/folders.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ARRAY=("$HOME/linguaggi_modelli_computazionali" "$HOME/datamining" "$HOME/cpvision" "$HOME/gip" "$HOME/scripts/notes" "$HOME/homelab/notes" "$HOME/youcook/notes" "$HOME/curiel_bot/notes")
