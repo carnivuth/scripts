5fd8d2754d0f1187a34ee34fba58d84b1d4362f6
carnivuth <matti200042@gmail.com>
2024-01-13 16:12:06 +0100

    added support for multiple formats

---
+++ utilities/convert_to.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# help
+if [[ "$1" == "--help" ]]; then
+    echo "small script to convert file formats in batch with ffmpeg"
+    echo "parameters: src-folder dest-folder format"
+    exit 0
+fi
+# exit if wrong parameters
+if [[ "$#" -ne 3 ]]; then
+    echo "wrong parameters, parameters are:"
+    echo "src-folder dest-folder format"
+    exit 1
+fi
+
+SRC_FOLDER="$1"
+DEST_FOLDER="$2"
+FORMAT="$3"
+
+
+if [[ ! -d "$SRC_FOLDER" ]]; then
+    echo "first parameter must be a folder"
+    exit 1
+fi
+
+# create folder if dosent exist
+if [[ ! -d "$DEST_FOLDER" ]]; then
+    mkdir -p "$DEST_FOLDER"
+fi
+
+for FILE in $SRC_FOLDER/*; do
+
+    # get name file
+    NAME="$(echo $FILE | rev | cut -d"/" -f1 |cut -d"." -f2| rev | awk  '{$1=$1};1')"
+
+    # encode with ffmpeg
+    ffmpeg -v quiet -y -stats -i "$FILE" "$DEST_FOLDER/$NAME.$FORMAT"
+done
