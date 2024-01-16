8623c80165ac9dbcd5f83f918e5486dabd79a503
carnivuth <matti200042@gmail.com>
2024-01-16 10:22:59 +0100

    added print

--- utilities/update_vaults.sh
+++ utilities/update_vaults.sh
@@ -2,5 +2,7 @@
 # documentation at https://github.com/carnivuth/scripts/blob/main/notes/pages/UPDATE_VAULTS.md
 source "$HOME/scripts/settings.sh"
 cat $SCRIPTS_HOME_FOLDER/utilities/vaults | while read vault; do
-$SCRIPTS_HOME_FOLDER/utilities/create_obsidian_vault.sh "$vault" --reset
+    
+    echo "updating $vault"
+    $SCRIPTS_HOME_FOLDER/utilities/create_obsidian_vault.sh "$vault" --reset
 done
