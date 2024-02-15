962c121a327a3b808a12a93ea429e676e5522ae2
carnivuth <matti200042@gmail.com>
2024-02-15 16:44:33 +0100

    added support for downloading also videos

--- utilities/yt-downloader.sh
+++ utilities/yt-downloader.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
 if [[ "$1" == "--help" ]]; then
-    echo 'parameters dest-folder URL'
+    echo 'script for downloading playlist from youtube'
+    echo 'usage:'
+    echo "$0" "dest-folder" "URL" "[-a]"
     exit 0
 fi
-if [[ "$#" -ne 2 ]]; then
+
+# check for number of parameters
+if [[ "$#" -lt 2 ]]; then
     echo "wrong parameters"
     exit 1
 fi
-yt-dlp --ignore-errors --continue --no-overwrites --download-archive progress.txt -x -P "$1" -f bestaudio "$2"
+
+DEST_FOLDER="$1"
+URL="$2"
+
+shift
+shift
+
+# get option parameters
+while getopts a: flag; do
+    case "${flag}" in
+    a) AUDIO_ONLY="-x -f bestaudio" ;;
+    esac
+done
+
+# run yt-dlp
+YT_DLP_CMD="yt-dlp \
+    --ignore-errors \
+    --continue \
+    --no-overwrites \
+    --download-archive progress.txt \
+    ${AUDIO_ONLY} \
+    -P $DEST_FOLDER \
+    $URL"
+
+echo "running $YT_DLP_CMD"
+
+$YT_DLP_CMD
+
 rm progress.txt
