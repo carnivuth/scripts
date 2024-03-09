77be7f023b5561283026f76312850d29988b464a
carnivuth <matti200042@gmail.com>
2024-03-09 14:39:06 +0100

    fixed lock bug

--- carnivuth-DE/hyprland-setup/lvim/config.lua
+++ carnivuth-DE/hyprland-setup/lvim/config.lua
@@ -29,3 +29,10 @@ vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "yamlls" })
 lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
   return server ~= "ansiblels"
 end, lvim.lsp.automatic_configuration.skipped_servers)
+
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Terminal",
+  f = { "<cmd>ToggleTerm<cr>", "Floating terminal" },
+  v = { "<cmd>2ToggleTerm size=30 direction=vertical<cr>", "Split vertical" },
+  h = { "<cmd>2ToggleTerm size=10 direction=horizontal<cr>", "Split horizontal" },
+}
