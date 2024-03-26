e0c45c91ff7b8ec86c10f8237901db9e02718615
carnivuth <matti200042@gmail.com>
2024-03-26 12:03:59 +0100

    fixed installation error when links exists

--- desktop-files/install.sh
+++ desktop-files/install.sh
@@ -4,4 +4,4 @@ source "$HOME/scripts/settings.sh"
 # copy config files
 echo 'coping desktop files'
 mkdir -p "$HOME/.local/share/applications/"
-ln -s  "$SCRIPTS_HOME_FOLDER/desktop-files/"*.desktop "$HOME/.local/share/applications/"
+ln -fs  "$SCRIPTS_HOME_FOLDER/desktop-files/"*.desktop "$HOME/.local/share/applications/"
