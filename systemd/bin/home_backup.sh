b269ceb2130be71e212ae44368e89fc05db559cf
carnivuth <matti200042@gmail.com>
2024-03-28 16:27:05 +0100

    refactor systemd services

--- systemd/bin/home_backup.sh
+++ systemd/bin/home_backup.sh
@@ -2,9 +2,6 @@
 #make backups with borg 
 #documentation at https://github.com/carnivuth/scripts/blob/main/notes/pages/HOME_BACKUP.md
 source "$HOME/scripts/settings.sh"
-source "$SCRIPTS_LIBS_FOLDER/array_importer.sh"
-
-array_importer "$SCRIPTS_HOME_FOLDER/systemd/scripts/target.sh" "$HOME" 
 
 #folder where backups are stored
 BACKUP_FOLDER="/storage/borg"
@@ -18,7 +15,7 @@ fi
 
 export BORG_PASSCOMMAND="secret-tool lookup borg-repository home"
 
-for TARGET in ${ARRAY[@]}; do 
+for TARGET in "$folder_to_backup" do 
     NAME="$(echo $TARGET | rev | cut -d '/' -f 1 | rev)"
     borg create --info "$BACKUP_FOLDER::$NAME-$(date +%c)" "$TARGET" 2>> "$SCRIPTS_LOGS_FOLDER/home_backup.logs" >> "$SCRIPTS_LOGS_FOLDER/home_backup.logs" 
  done
