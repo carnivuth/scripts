c74032e81d379b99118af49a042c394459da87ca
Carnivuth <matti200042@gmail.com>
2024-04-28 18:32:50 +0200

    added systemd units installation

--- systemd/install.sh
+++ systemd/install.sh
@@ -25,3 +25,25 @@ for servicefile in "$SCRIPTS_HOME_FOLDER/systemd/services/"*.service; do
     systemctl --user enable --now $service
   fi
 done
+
+# installing systemd timers
+cp -rf "$SCRIPTS_HOME_FOLDER/systemd/timers/"*.service "$HOME/.config/systemd/user/"
+cp -rf "$SCRIPTS_HOME_FOLDER/systemd/timers/"*.timer "$HOME/.config/systemd/user/"
+
+# setting basolute paths
+sed -i "s|\[\[HOME\]\]|${HOME}|g" "$HOME/.config/systemd/user/"*.service
+
+# reload daemons
+systemctl --user daemon-reload
+
+# enable all services
+for timerfile in "$SCRIPTS_HOME_FOLDER/systemd/timers/"*.timer; do
+  timer=$(echo $timerfile | rev | cut -d'/' -f 1 | rev)
+
+  # enable timer if is present in config variable
+  if [[ "$ENABLED_TIMERS" =~ "$timer" ]]; then
+    echo "enabling $timer"
+    systemctl --user enable --now $timer
+  fi
+done
+ echo "if you enable the backup service remember to run $HOME/scripts/systemd/bin/backup.sh init to initialize the borg repo"
