1e43ac1db1a38b60f02ca4563cb0c8c6b1a5a595
carnivuth <matti200042@gmail.com>
2024-08-01 11:54:02 +0200

    removed path editing

--- utilities/install.sh
+++ utilities/install.sh
@@ -5,10 +5,3 @@ source "$HOME/scripts/settings.sh"
 echo 'installing packages'
 packets='fzf sshpass yt-dlp ffmpeg gawk'
 sudo pacman -S $packets
-
-# copy config files
-echo 'installing utils'
-#if [[ "$(cat $HOME/.bashrc | grep $SCRIPTS_HOME_FOLDER/utilities/)" == "" ]]; then
-#    echo "export PATH=$SCRIPTS_HOME_FOLDER/utilities/:"'$PATH' >>"$HOME/.bashrc"
-#    export PATH="$SCRIPTS_HOME_FOLDER/utilities/:$PATH"
-#fi
