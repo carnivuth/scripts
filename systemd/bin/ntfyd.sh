9674934cee199aa19edde7a3de2381e838c94eba
carnivuth <matti200042@gmail.com>
2024-08-02 12:08:38 +0200

    added download attachments support

--- systemd/bin/ntfyd.sh
+++ systemd/bin/ntfyd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 source "$HOME/scripts/settings.sh"
 
 get_param(){
@@ -16,6 +17,43 @@ help_command(){
 stop_command(){
   rm -f "$NTFYD_MESSAGE_BUFFER"
 }
+handle_message(){
+
+        MSG="$1"
+        message="$(get_param "$MSG" "message")"
+        title="$(get_param "$MSG" "title")"
+        attachment="$(get_param "$MSG" "attachment")"
+
+        echo $message
+        echo $title
+        echo $attachment
+
+        notification_body=""
+        notification_params=""
+
+        if [[ "$title" != "null" ]]; then
+          notification_body="$notification_body $title $message"
+        else
+          notification_body="$notification_body $message"
+        fi
+
+        if [[ "$attachment" != "null" ]]; then
+
+          attachment_url="$(get_param "$attachment" "url" | sed 's|\(.*\)/file/\(.*\)|\1/ntfy/file/\2|g' )"
+          echo "$attachment_url"
+
+          notification_body="$notification_body click to download stdout"
+          notification_params=--action='download=download attachment'
+        fi
+
+        echo $notification_body
+        result="$(notify-send -a "$NTFYD_APP_NAME_NOTIFICATION" -u "normal" $notification_params "$notification_body")"
+        if [[ "$result" == 'download' ]]; then
+                xdg-open "$attachment_url"
+        fi
+}
+
+
 
 start_command(){
 
@@ -36,17 +74,8 @@ start_command(){
     case "$event" in
 
       "message")
-
-        message="$(get_param "$MSG" "message")"
-        title="$(get_param "$MSG" "title")"
-
-        if [[ "$title" != "null" ]]; then
-          notify-send -a "$NTFYD_APP_NAME_NOTIFICATION" -u "normal" "$title $message"
-        else
-          notify-send -a "$NTFYD_APP_NAME_NOTIFICATION" -u "normal" "$message"
-        fi
-        ;;
-
+              handle_message "$MSG"
+              ;;
     esac
 
   done
