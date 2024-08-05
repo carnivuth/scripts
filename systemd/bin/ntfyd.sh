439740f711ff3e9b39b434ffee319a31b351cf0e
carnivuth <matti200042@gmail.com>
2024-08-05 13:19:20 +0200

    general improvements

--- systemd/bin/ntfyd.sh
+++ systemd/bin/ntfyd.sh
@@ -6,67 +6,67 @@ get_param(){
 }
 
 help_command(){
-        echo "usage $0 [start]"
+  echo "usage $0 [start]"
 }
 
 handle_message(){
 
-        MSG="$1"
-        message="$(get_param "$MSG" "message")"
-        title="$(get_param "$MSG" "title")"
-        attachment="$(get_param "$MSG" "attachment")"
+  MSG="$1"
+  message="$(get_param "$MSG" "message")"
+  title="$(get_param "$MSG" "title")"
+  attachment="$(get_param "$MSG" "attachment")"
 
-        echo $message
-        echo $title
-        echo $attachment
+  echo "$message"
+  echo "$title"
+  echo "$attachment"
 
-        notification_body=""
-        notification_params=""
+  notification_body=""
+  notification_params=""
 
-        if [[ "$title" != "null" ]]; then
-          notification_body="$notification_body $title $message"
-        else
-          notification_body="$notification_body $message"
-        fi
+  if [[ "$title" != "null" ]]; then
+    notification_body="$notification_body $title $message"
+  else
+    notification_body="$notification_body $message"
+  fi
 
-        if [[ "$attachment" != "null" ]]; then
+  if [[ "$attachment" != "null" ]]; then
 
-          attachment_url="$(get_param "$attachment" "url" | sed 's|\(.*\)/file/\(.*\)|\1/ntfy/file/\2|g' )"
-          echo "$attachment_url"
+    attachment_url="$(get_param "$attachment" "url" | sed 's|\(.*\)/file/\(.*\)|\1/ntfy/file/\2|g' )"
+    echo "$attachment_url"
 
-          notification_body="$notification_body click to download stdout"
-          notification_params=--action='download=download attachment'
-        fi
+    notification_body="$notification_body click to download stdout"
+    notification_params=--action='download=download attachment'
+  fi
 
-        echo $notification_body
-        result="$(notify-send -a "$NTFYD_APP_NAME_NOTIFICATION" -u "normal" $notification_params "$notification_body")"
-        if [[ "$result" == 'download' ]]; then
-                xdg-open "$attachment_url"
-        fi
+  echo "$notification_body"
+  result="$(notify-send -a "$NTFYD_APP_NAME_NOTIFICATION" -u "normal" $notification_params "$notification_body")"
+  if [[ "$result" == 'download' ]]; then
+    xdg-open "$attachment_url"
+  fi
 }
 
 start_command(){
 
-  stdbuf -oL  curl -s "$NTFYD_ENDPOINT/$NTFYD_TOPICS/json" | while read MSG; do
+  stdbuf -oL  curl -s "$NTFYD_ENDPOINT/$NTFYD_TOPICS/json" | while read -r MSG; do
 
-    event="$(get_param "$MSG" "event")"
-    id="$(get_param "$MSG" "id")"
-    time="$(get_param "$MSG" "time")"
-    topic="$(get_param "$MSG" "topic")"
+  event="$(get_param "$MSG" "event")"
+  id="$(get_param "$MSG" "id")"
+  time="$(get_param "$MSG" "time")"
+  topic="$(get_param "$MSG" "topic")"
 
-    echo "$topic"
-    echo "$event"
-    echo "$id"
-    echo "$time"
+  echo "$topic"
+  echo "$event"
+  echo "$id"
+  echo "$time"
 
-    case "$event" in
+  case "$event" in
 
-      "message")
-              handle_message "$MSG"
-              ;;
-    esac
+    "message")
+      handle_message "$MSG"
+      ;;
+  esac
 
-  done
+done
 }
 
 case "$1" in
