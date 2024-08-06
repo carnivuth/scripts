2307b28ce5d2a8ad2e514faf9f51153a039aeb7a
carnivuth <matti200042@gmail.com>
2024-08-06 17:18:20 +0200

    fixed theme setup

--- bin/window_switcher_wofi.sh
+++ bin/window_switcher_wofi.sh
@@ -2,7 +2,7 @@
 source "$HOME/scripts/settings.sh"
 source "$SCRIPTS_LIB_FOLDER"/"$MENU_BACKEND"_standard.sh
 if [[ "$XDG_CURRENT_DESKTOP" != 'Hyprland' ]]; then echo 'not in hyprland session'; exit 1; fi
-menu_theme_setup "window-switcher-wofi"
+menu_theme_setup "window_switcher_wofi"
 prompt="window switcher"
 # print menu filtering elements with jq
 chosen="$(hyprctl clients -j | jq -r '.[].title | select(length > 0)'| menu_cmd "$prompt")"
