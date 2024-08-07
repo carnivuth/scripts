bdc078a633ee7bb1511f85d26b8fafd2266dfd95
carnivuth <matti200042@gmail.com>
2024-08-07 21:34:03 +0200

    refactor launcher for launching sites from json file instead of desktopfile

--- bin/launcher.sh
+++ bin/launcher.sh
@@ -6,36 +6,25 @@ source "$SCRIPTS_LIB_FOLDER/print_bins.sh"
 MENU_NAME="launcher"
 PROMPT="programs"
 
-
 help_message(){
-  echo "launcher for binaries and desktop files "
+  echo "launcher for binaries and web applications"
 }
+
 list_elements_to_user(){
   apps="$( print_bins )"
-  apps="$(jq -r '.app' "$SCRIPTS_LOCAL_FOLDER/sites.json") $apps "
+  apps="$(jq -r '.[].site' "$SCRIPTS_HOME_FOLDER/sites.json") $apps "
   echo $apps | sed  's/ /\n/g'
 }
 
 exec_command_with_chosen_element(){
   if [[ ! -x "$( which "$1" 2>/dev/null)" ]];then
-    site="$( jq -r "select(.app == \"$1\")| .link " "$SCRIPTS_LOCAL_FOLDER/sites.json" )"
+    site="$( jq -r ".[] | select(.site == \"$1\")| .link " "$SCRIPTS_HOME_FOLDER/sites.json" )"
     if [[ "$site" != '' ]];then
       launch_webapp "$site"
     fi
   else
     exec "$1"
   fi
-}
-
-# create json structure from desktop files
-desktop_files_to_json(){
-  ls "$SCRIPTS_DESKTOPFILES_FOLDER" | while read -r filename; do
-  app="$(basename "$SCRIPTS_DESKTOPFILES_FOLDER/$filename"  | awk -F'.' '{print $1}')";
-  link="$(grep webapp "$SCRIPTS_DESKTOPFILES_FOLDER/$filename" | awk -F \' '{print $2}' )"
-  if [[ "$link" != '' ]] && [[ "$app" != '' ]]; then
-    echo "{ \"app\": \"$app\",\"link\": \"$link\"}"
-  fi
-done
 }
 
 source "$SCRIPTS_LIB_FOLDER/menu.sh"
