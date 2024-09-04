b2e39bdba609bf7bc2b2b9018cfa465b2bc7d84d
carnivuth <matti200042@gmail.com>
2024-09-04 14:56:26 +0200

    fixed script for footer

--- bin/obsidian_add_footer.sh
+++ bin/obsidian_add_footer.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
 
-#set -x
-function create_footer(){
-  file="$1"
-  message=""
-  PREC=""
-  NEXT=""
-  shift
-while getopts p:n: flag; do
-  case "${flag}" in
-    p) PREC=${OPTARG} ;shift;shift ;;
-    n) NEXT=${OPTARG} ;shift;shift ;;
-    *) echo "flag $flag not supported"; exit 1 ;;
-  esac
-done
-  if [[ ! -f "$file" ]]; then return 1; fi
-  if [[ -f "$PREC" ]]; then
-    message="[PREVIOUS]($PREC)"
-  fi
-  if [[ -f "$NEXT" ]]; then
-    message="$message [NEXT]($NEXT) "
-  fi
-  echo "$message"
-  if [[ "$(grep 'NEXT' "$file")" == '' ]] && [[ "$(grep 'PREVIOUS' "$file")" == '' ]]; then
-    echo  "[PREVIOUS]($PREC) [NEXT]($NEXT)" >> $file
-  fi
-}
-
 # loop files in dir
 for file in *.md; do
   index=''
@@ -34,22 +7,31 @@ for file in *.md; do
   next=''
   prec_file=""
   next_file=""
+  message=""
   # extract index props
   index="$( grep 'index:' "$file" |awk -F ' ' '{print $2}')"
   if [[ index != "" ]];then
     # compute prec value and next value
     prec=$(($index - 1))
     next=$(($index + 1))
-     echo "index: $index"
-     echo "prec: $prec"
-     echo "next: $next"
+    echo "prec: $prec"
+    echo "index: $index"
+    echo "next: $next"
 
-  # grep for prec value and next value
-  prec_file="$( grep -l "index: $prec" *.md)"
-  next_file="$( grep -l "index: $next" *.md)"
-  # create link for prec and next if founded
-  echo "create_footer $file -p $prec_file -n $next_file"
-  create_footer $file -p $prec_file -n $next_file
+    # grep for prec value and next value
+    prec_file="$( grep -l "index: $prec" *.md)"
+    next_file="$( grep -l "index: $next" *.md)"
+
+    if [[ -f "$prec_file" ]]; then
+      message="[PREVIOUS]($prec_file)"
+    fi
+    if [[ -f "$next_file" ]]; then
+      message="$message [NEXT]($next_file)"
+    fi
+    echo "$message"
+    if [[ "$(grep 'NEXT' "$file")" == '' ]] && [[ "$(grep 'PREVIOUS' "$file")" == '' ]]; then
+      echo  "$message" >> $file
+    fi
   fi
 
 done
