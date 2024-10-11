a9b5f3c6ed55e60a05bb6c2e0210b31a2c688689
carnivuth <matti200042@gmail.com>
2024-10-11 12:23:47 +0200

    added utility for merge pdf

---
+++ bin/merge_pdf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [[ $# -lt 3 ]]; then echo "pass at least an outfile and 2 files to merge"; exit 1; fi
+if [[ -f $1 ]]; then echo "outfile $1 already exists"; exit 1; fi
+OUTFILE="$1"
+shift
+gs \
+  -dBATCH\
+  -dNOPAUSE\
+  -q\
+  -sDEVICE=pdfwrite\
+  -dPDFSETTINGS=/prepress\
+  -sOutputFile="$OUTFILE" "$@"
+
