7d80fd20526e1a276126f6cd6cbb520a1eca94df
carnivuth <mlonghi@labs.it>
2024-10-21 10:09:52 +0200

    added catpuccin theme

--- lib/bemenu_standard.sh
+++ lib/bemenu_standard.sh
@@ -4,6 +4,7 @@ source "$SCRIPTS_LIB_FOLDER/get_wal_color.sh"
 
 get_wal_color
 BEMENU_HIGH=21
+BEMENU_BORDER=1
 BEMENU_PADDING=10
 BEMENU_FONT='JetBrainsMono Nerd Font,JetBrainsMono NF:style=ExtraBold 9'
 
@@ -15,15 +16,26 @@ menu_theme_setup(){
 menu_cmd() {
   bemenu \
     -p "$1"\
-    --hb "${COLORS[13]}"\
-    --hf "${COLORS[0]}"\
-    --ff "${COLORS[13]}"\
-    --fb "${COLORS[0]}"\
-    --tf "${COLORS[13]}"\
-    --tb "${COLORS[0]}"\
-    --nf "${COLORS[13]}"\
-    --nb "${COLORS[0]}"\
+    --fb "#24273a"\
+    --ff "#cad3f5"\
+    --nb "#24273a"\
+    --nf "#cad3f5"\
+    --tb "#24273a"\
+    --hb "#24273a"\
+    --tf "#ed8796"\
+    --hf "#eed49f"\
+    --af "#cad3f5"\
+    --ab "#24273a"\
     --hp "$BEMENU_PADDING"\
+    -B "$BEMENU_BORDER"\
     --fn "$BEMENU_FONT"\
     -H "$BEMENU_HIGH"
+    #--hb "${COLORS[13]}"\
+    #--hf "${COLORS[0]}"\
+    #--ff "${COLORS[13]}"\
+    #--fb "${COLORS[0]}"\
+    #--tf "${COLORS[13]}"\
+    #--tb "${COLORS[0]}"\
+    #--nf "${COLORS[13]}"\
+    #--nb "${COLORS[0]}"\
   }
