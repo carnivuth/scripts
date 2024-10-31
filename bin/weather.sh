427ca7caa4a5b1f08ff6faf97fa28a2e65c4fab3
carnivuth <matti200042@gmail.com>
2024-10-31 09:50:40 +0100

    refactor weather to independent service

--- bin/weather.sh
+++ bin/weather.sh
@@ -2,15 +2,45 @@
 # get_weather.sh
 source "$HOME/.config/scripts/settings.sh"
 
-# get data from wttr
-text=$(curl -s "https://wttr.in/$WEATHER_LOCATION?d&format=3")
+WEATHER_SLEEP_TIME="10m"
+HELP_MESSAGE="usage $0 [start|get_weather]"
 
-if [[ $? == 0 ]]; then
-    #text=$(echo "$text" | sed -E "s/\s+/ /g")
-    tooltip=$(curl -s "https://wttr.in/$WEATHER_LOCATION?format=4")
+function get_weather(){
+  if [[ ! -f "$SCRIPTS_RUN_FOLDER/weather.json" ]];then echo "no weather to display, is the daemon running?"; exit 1; fi
+  cat "$SCRIPTS_RUN_FOLDER/weather.json"
+}
+
+function pre_stop(){
+  rm -f "$SCRIPTS_RUN_FOLDER/weather.json"
+}
+
+
+function start(){
+  while true; do
+    # get data from wttr
+    text=$(curl -s "https://wttr.in?format=3")
     if [[ $? == 0 ]]; then
-        #tooltip=$(echo "$tooltip" | sed -E "s/\s+/ /g")
-        echo "{\"text\":\"$text\", \"tooltip\":\"$tooltip\"}"
-        exit
+      tooltip=$(curl -s "https://wttr.in?format=4")
+      if [[ $? == 0 ]]; then
+        echo "{\"text\":\"$text\", \"tooltip\":\"$tooltip\"}" > "$SCRIPTS_RUN_FOLDER/weather.json"
+      fi
     fi
-fi
+    sleep $WEATHER_SLEEP_TIME
+  done
+}
+
+
+case "$1" in
+  "start")
+    start
+    ;;
+  "pre_stop")
+    pre_stop
+    ;;
+  "get_weather")
+    get_weather
+    ;;
+  *)
+    echo "$HELP_MESSAGE"
+    ;;
+esac
