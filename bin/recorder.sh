af5946c65254dfd0ec05b54a68b107adc8d97b60
carnivuth <matti200042@gmail.com>
2024-11-23 15:03:07 +0100

    fixed recorder script, improved bindings for readability

--- bin/recorder.sh
+++ bin/recorder.sh
@@ -5,31 +5,24 @@ source "$HOME/.config/scripts/settings.sh"
 
 mkdir -p "$RECORDER_OUTPUT_PATH"
 
-start_command(){
-  name="$(date +%s).mp4"
-  echo "$name" >> "$SCRIPTS_LOGS_FOLDER/recorder.log" 2>&1
-  notify-send -a "$RECORDER_APP_NAME_NOTIFICATION" -u "normal" "start recording"
-  "$RECORDER" -f "$RECORDER_OUTPUT_PATH/$name" >> "$SCRIPTS_LOGS_FOLDER/recorder.log" 2>&1 &
-}
-
-stop_command(){
+toggle(){
   pid="$(pidof $RECORDER)"
   if [[ "$pid" != "" ]]; then
     kill "$pid" >> "$SCRIPTS_LOGS_FOLDER/recorder.log" 2>&1
     notify-send -a "$RECORDER_APP_NAME_NOTIFICATION" -u "normal" "stop recording"
+  else
+    name="$(date +%s).mp4"
+    echo "$name" >> "$SCRIPTS_LOGS_FOLDER/recorder.log" 2>&1
+    notify-send -a "$RECORDER_APP_NAME_NOTIFICATION" -u "normal" "start recording"
+    "$RECORDER" -f "$RECORDER_OUTPUT_PATH/$name" >> "$SCRIPTS_LOGS_FOLDER/recorder.log" 2>&1 &
   fi
-  echo ""
 }
 
 case "$1" in
-  "start")
-    stop_command
-    start_command
-    ;;
-  "stop")
-    stop_command
+  "toggle")
+    toggle
     ;;
   *)
-    echo "usage $0 [start|stop]"
+    echo "usage $0 [toggle]"
     ;;
 esac
