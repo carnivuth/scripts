d38dc55235629a8f3493259b11cd158e717606e2
carnivuth <matti200042@gmail.com>
2024-12-21 22:05:56 +0100

    fixed launch  webapp window

--- lib/launch_webapp.sh
+++ lib/launch_webapp.sh
@@ -2,14 +2,14 @@
 
 launch_webapp(){
   source "$HOME/.config/scripts/settings.sh"
-  
+
   firefox --new-tab "$1"
-  
+
   # draw attention to the firefox window based on window manager
   if [[ "$XDG_CURRENT_DESKTOP" == 'Hyprland' ]]; then
-    hyprctl dispatch 'focuswindow firefox'
+    hyprctl dispatch 'focuswindow class:firefox'
   fi
-  
+
   if [[ "$XDG_CURRENT_DESKTOP" == 'i3' ]]; then
     i3-msg '[class="firefox"] focus'
   fi
