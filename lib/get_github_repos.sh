d8dd2c1326faf46d2c9a0db3d50bf1666da590fc
carnivuth <matti200042@gmail.com>
2025-01-19 12:36:43 +0100

    improved comments

--- lib/get_github_repos.sh
+++ lib/get_github_repos.sh
@@ -4,10 +4,13 @@ get_file(){
   curl "https://api.github.com/users/$GITHUB_REPOVIEWER_ACCOUNT/repos?per_page=100" > "$SCRIPTS_LOCAL_FOLDER/repos.json"
 }
 function get_github_repos(){
+
   # check if cache is valid
   if [[ ! -f "$SCRIPTS_LOCAL_FOLDER/repos.json" ]] || [[ "$(wc -l "$SCRIPTS_LOCAL_FOLDER/repos.json")" == "0" ]]; then get_file; fi
   birth_date="$(stat -c '%W' "$SCRIPTS_LOCAL_FOLDER/repos.json" )"
   today="$(date +%s)"
+
+  # refresh cache if is too old
   if [[ "$(($today - $birth_date))" -gt 86400  ]]; then
     rm "$SCRIPTS_LOCAL_FOLDER/repos.json"
     get_file
