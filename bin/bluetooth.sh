e93b2e86a5dab9a3578fdc7936e0c13d39ed0f22
carnivuth <matti200042@gmail.com>
2025-02-04 10:51:35 +0100

    refactor with cli lib

--- bin/bluetooth.sh
+++ bin/bluetooth.sh
@@ -1,62 +1,60 @@
 #!/bin/bash
+source "$HOME/.config/scripts/settings.sh"
+declare -A FLAGS
+FLAGS[ciw]="ciw"
+FLAGS_STRING=''
+
+declare -A COMMANDS
+COMMANDS[bluetooth_print]="print bluetooth devices"
+COMMANDS[bluetooth_toggle]="toggle bluetooth"
 
 bluetooth_all_connect(){
-        devices_paired=$(bluetoothctl devices Paired | grep Device | cut -d ' ' -f 2)
-        echo "$devices_paired" | while read -r line; do
-            bluetoothctl connect "$line" >> /dev/null
-       done
+  devices_paired=$(bluetoothctl devices Paired | grep Device | cut -d ' ' -f 2)
+  echo "$devices_paired" | while read -r line; do
+  bluetoothctl connect "$line" >> /dev/null
+done
 }
 
 bluetooth_print() {
-    bluetoothctl | while read -r; do
-        if [ "$(systemctl is-active "bluetooth.service")" == "active" ] && [ "$(bluetoothctl show | grep -o "Powered: yes")" == "Powered: yes" ]; then
-            echo 'On'
-            devices_paired=$(bluetoothctl devices Paired | grep Device | cut -d ' ' -f 2)
-            counter=0
+  bluetoothctl | while read -r; do
+  if [ "$(systemctl is-active "bluetooth.service")" == "active" ] && [ "$(bluetoothctl show | grep -o "Powered: yes")" == "Powered: yes" ]; then
+    echo 'On'
+    devices_paired=$(bluetoothctl devices Paired | grep Device | cut -d ' ' -f 2) counter=0
 
-            for device in $devices_paired; do
-                device_info=$(bluetoothctl info "$device")
+    for device in $devices_paired; do
+      device_info=$(bluetoothctl info "$device")
 
-                if echo "$device_info" | grep -q "Connected: yes"; then
-                    device_alias=$(echo "$device_info" | grep "Alias" | cut -d ' ' -f 2-)
+      if echo "$device_info" | grep -q "Connected: yes"; then
+        device_alias=$(echo "$device_info" | grep "Alias" | cut -d ' ' -f 2-)
 
-                    if [ $counter -gt 0 ]; then
-                        printf ", %s" "$device_alias "
-                    else
-                        printf " %s" "$device_alias "
-                    fi
-
-                    counter=$((counter + 1))
-                fi
-            done
-
+        if [ $counter -gt 0 ]; then
+          printf ", %s" "$device_alias "
         else
-            echo 'Off'
-
+          printf " %s" "$device_alias "
         fi
+
+        counter=$((counter + 1))
+      fi
     done
+
+  else
+    echo 'Off'
+
+  fi
+done
 }
 
 bluetooth_toggle() {
-    if bluetoothctl show | grep -q "Powered: no"; then
-        bluetoothctl power on >> /dev/null
-#        sleep 1
-#        bluetooth_all_connect
-    else
-        devices_paired=$(bluetoothctl devices Paired | grep Device | cut -d ' ' -f 2)
-        echo "$devices_paired" | while read -r line; do
-            bluetoothctl disconnect "$line" >> /dev/null
-        done
+  if bluetoothctl show | grep -q "Powered: no"; then
+    bluetoothctl power on >> /dev/null
+  else
+    devices_paired=$(bluetoothctl devices Paired | grep Device | cut -d ' ' -f 2)
+    echo "$devices_paired" | while read -r line; do
+    bluetoothctl disconnect "$line" >> /dev/null
+  done
 
-        bluetoothctl power off >> /dev/null
-    fi
+  bluetoothctl power off >> /dev/null
+  fi
 }
 
-case "$1" in
-    toggle)
-        bluetooth_toggle
-        ;;
-    *)
-        bluetooth_print
-        ;;
-esac
+source "$SCRIPTS_LIB_FOLDER/cli.sh"
