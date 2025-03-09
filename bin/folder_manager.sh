ffc1cca5518ca3454b8397b36edeeb3389c0ff32
carnivuth <matti200042@gmail.com>
2025-03-09 12:17:06 +0100

    added folder manager script together with systemd parametric unit

---
+++ bin/folder_manager.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+source "$HOME/.config/scripts/settings.sh"
+
+declare -A FLAGS
+FLAGS[d]='DIRECTORY=${OPTARG}'
+FLAGS_STRING='d:'
+
+declare -A COMMANDS
+COMMANDS[run]="run a cleanup operation"
+COMMANDS[watch]="watch a folder for changes and run cleanup operations"
+
+function run(){
+
+  if [[ -z $DIRECTORY ]]; then echo "set DIRECTORY with -d option";exit 1; fi
+  if [[ ! -d "$DIRECTORY" ]]; then echo "$DIRECTORY is not a DIRECTORY";exit 1; fi
+
+  declare -A DIRS
+  DIRS[png]="images"
+  DIRS[jpg]="images"
+  DIRS[tar]="compressed"
+  DIRS[gz]="compressed"
+  DIRS[zip]="compressed"
+  DIRS[pdf]="documents"
+  DIRS[docx]="documents"
+  DIRS[txt]="documents"
+
+  for file in $DIRECTORY/* ; do
+    if [[ -f "$file" ]]; then
+
+      filename="$(basename "$file")"
+      extension="${filename##*.}"
+      dest_dir=${DIRS[$extension]}
+
+      if [[ -n $dest_dir ]]; then
+        mkdir -p "$DIRECTORY/$dest_dir"
+        echo "moving $file in $DIRECTORY/$dest_dir"
+        mv "$file" "$DIRECTORY/$dest_dir"
+      fi
+    fi
+  done
+}
+
+function watch(){
+
+  if [[ -z $DIRECTORY ]]; then echo "set DIRECTORY with -d option";exit 1; fi
+  if [[ ! -d "$DIRECTORY" ]]; then echo "$DIRECTORY is not a DIRECTORY";exit 1; fi
+
+  echo "started watching for changes in $DIRECTORY"
+  inotifywait -e create -m "$DIRECTORY" | while read directory filename; do
+    if run; then
+      notify-send -i "$FOLDER_MANAGER_NOTIFY_ICON" -a "$FOLDER_MANAGER_APP_NAME_NOTIFICATION" -u "normal"  "reorganized $DIRECTORY"
+    fi
+  done
+}
+
+source "$SCRIPTS_LIB_FOLDER/cli.sh"
