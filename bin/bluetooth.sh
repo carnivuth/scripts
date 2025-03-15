ebc5219aa07a53b7380d7dbda4f2f24522afbafe
carnivuth <matti200042@gmail.com>
2025-03-15 21:54:18 +0100

    merged bluetooth menus

--- bin/bluetooth.sh
+++ bin/bluetooth.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 source "$HOME/.config/scripts/settings.sh"
 declare -A FLAGS
-FLAGS[ciw]="ciw"
-FLAGS_STRING=''
+FLAGS["b"]='MENU_BACKEND=${OPTARG}'
+FLAGS_STRING='b:'
 
 declare -A COMMANDS
 COMMANDS[bluetooth_print]="print bluetooth devices"
 COMMANDS[bluetooth_toggle]="toggle bluetooth"
+COMMANDS[bluetooth_connect]="connect to a bluetooth device"
 
-bluetooth_all_connect(){
+function bluetooth_all_connect(){
   devices_paired=$(bluetoothctl devices Paired | grep Device | cut -d ' ' -f 2)
   echo "$devices_paired" | while read -r line; do
   bluetoothctl connect "$line" >> /dev/null
 done
 }
 
-bluetooth_print() {
+function bluetooth_print() {
   bluetoothctl | while read -r; do
   if [ "$(systemctl is-active "bluetooth.service")" == "active" ] && [ "$(bluetoothctl show | grep -o "Powered: yes")" == "Powered: yes" ]; then
     echo 'On'
@@ -44,7 +45,7 @@ bluetooth_print() {
 done
 }
 
-bluetooth_toggle() {
+function bluetooth_toggle() {
   if bluetoothctl show | grep -q "Powered: no"; then
     bluetoothctl power on >> /dev/null
   else
@@ -56,5 +57,33 @@ bluetooth_toggle() {
   bluetoothctl power off >> /dev/null
   fi
 }
+
+function bluetooth_connect(){
+
+  MENU_NAME="bluetoothconnect"
+  PROMPT="bluetooth devices"
+
+  function help_message(){
+    echo "script to connect to bluetooth devices"
+    echo " usage $0 "
+  }
+
+  function list_elements_to_user(){
+    bluetoothctl devices |  cut -f 2- -d' ' | sort | while read mac name;  do echo "$name";done
+  }
+
+  function exec_command_with_chosen_element(){
+    bluetoothctl devices |  cut -f 2- -d' ' | sort | while read mac name;  do
+    if [[ "$chosen" == "$name" ]]; then
+      bluetoothctl connect "$mac"
+    fi
+  done
+  }
+
+  source "$SCRIPTS_LIB_FOLDER/menu.sh"
+
+}
+
+
 
 source "$SCRIPTS_LIB_FOLDER/cli.sh"
