f524569db7b556034876bd0b8e8b1cd0a75f4ecf
Matteo Longhi <mlonghi@labs.it>
2025-03-17 10:07:24 +0100

    fixed bug on function names

--- bin/clean_env.sh
+++ bin/clean_env.sh
@@ -5,11 +5,11 @@ declare -A FLAGS
 FLAGS_STRING=''
 
 declare -A COMMANDS
-COMMANDS[docker]="clean docker env"
-COMMANDS[vagrant]="clean vagrant env"
-COMMANDS[ollama]="clean ollama env"
+COMMANDS[docker_env]="clean docker env"
+COMMANDS[vagrant_env]="clean vagrant env"
+COMMANDS[ollama_env]="clean ollama env"
 
-function docker(){
+function docker_env(){
   docker ps | awk -F' ' '{print $1}' | tail -n +2 | xargs docker container kill
   docker system prune -a -f
   docker image prune -a -f
@@ -17,7 +17,7 @@ function docker(){
   docker network prune -f
 }
 
-function vagrant(){
+function vagrant_env(){
   MACHINES="$(vagrant global-status --machine-readable | grep machine-id | awk -F , '{print $4}')"
 
   echo "$MACHINES" | while read machine ; do
@@ -26,7 +26,7 @@ function vagrant(){
 
 }
 
-function ollama(){
+function ollama_env(){
 
   ollama list | tail -n +2 | awk -F' ' '{print $1}' | while read model; do ollama rm "$model"; done
 }
