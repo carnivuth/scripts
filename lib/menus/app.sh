f1e3dd9fe6dbce2589033e2fd83b3ffd528e8447
Matteo Longhi <mlonghi@labs.it>
2025-04-28 16:10:28 +0200

    removed duplicates

--- lib/menus/app.sh
+++ lib/menus/app.sh
@@ -2,7 +2,7 @@
 source "$HOME/.config/scripts/settings.sh"
 
 list_app(){
-  grep -o -e '^Exec=.*' $HOME/.local/share/applications/* /usr/share/applications/*.desktop | awk -F'[ =]' '{print $2}' | awk -F'/' '{print $NF}' | sed 's/^/app:/g'
+  grep -o -e '^Exec=.*' $HOME/.local/share/applications/* /usr/share/applications/*.desktop | awk -F'[ =]' '{print $2}' | awk -F'/' '{print $NF}' | sort -u | sed 's/^/app:/g'
 }
 
 run_app(){
