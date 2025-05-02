0ab046eb9b4adc2f3fcb1df2b0482e3d0fe1d74e
Matteo Longhi <mlonghi@labs.it>
2025-05-02 16:56:43 +0200

    changed caffeine mode to event based model

--- bin/caffeine.sh
+++ bin/caffeine.sh
@@ -5,32 +5,58 @@ FLAGS_STRING=''
 declare -A FLAGS
 
 declare -A COMMANDS
-COMMANDS[toggle]="toggle connection to vpn configured in settings file"
-COMMANDS[check_status]="check if idle daemon is running"
+COMMANDS[toggle]="toggle caffeine mode"
+COMMANDS[on]="turn on caffein mode"
+COMMANDS[off]="turn off caffein mode"
+COMMANDS[check_status]="check caffeine mode status"
 
 if [[ "$DESKTOP_SESSION" == 'sway' ]];then idle_daemon="swayidle";fi
 if [[ "$DESKTOP_SESSION" == 'hyprland' ]];then idle_daemon="hypridle";fi
 
-toggle(){
+STATUS_FILE="$SCRIPTS_RUN_FOLDER/caffeine.status"
 
+off(){
+  if ! pgrep $idle_daemon;then
+    $idle_daemon &
+    echo 0 > "$STATUS_FILE"
+  fi
+}
+
+on(){
   if pgrep $idle_daemon;then
     kill -9 "$(pgrep $idle_daemon )"
+  fi
+  echo 1 > "$STATUS_FILE"
+}
+
+toggle(){
+  if pgrep $idle_daemon;then
+     on
   else
-    $idle_daemon &
+    off
   fi
 }
 
 function check_status(){
-  while true ; do
-  if pgrep $idle_daemon;then
-      text=" "
-      tooltip="caffeine mode off"
-    else
+  status="$(cat "$STATUS_FILE")"
+  if [[ "$status" == "1" ]];then
       text=" "
       tooltip="caffeine mode on"
+    else
+      text=" "
+      tooltip="caffeine mode off"
     fi
     echo "{ \"text\":\"$text\",\"tooltip\":\"$tooltip\"}" |jq --unbuffered --compact-output
-    sleep 5s
+  inotifywait -m -e close_write "$STATUS_FILE" | while read event; do
+  status="$(cat "$STATUS_FILE")"
+  if [[ "$status" == "1" ]];then
+      text=" "
+      tooltip="caffeine mode on"
+    else
+      text=" "
+      tooltip="caffeine mode off"
+    fi
+    echo "{ \"text\":\"$text\",\"tooltip\":\"$tooltip\"}" |jq --unbuffered --compact-output
   done
 }
 
