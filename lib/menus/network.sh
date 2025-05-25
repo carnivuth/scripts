20a0234deb342a648a9fa89ce716d9559173a632
carnivuth <matti200042@gmail.com>
2025-05-25 11:20:41 +0200

    fixed bug on connection

--- lib/menus/network.sh
+++ lib/menus/network.sh
@@ -2,13 +2,17 @@
 source "$HOME/.config/scripts/settings.sh"
 source "$SCRIPTS_LIB_FOLDER/notify.sh"
 
+APP_NAME="network"
+APP_ICON="/usr/share/icons/Papirus/16x16/apps/gnome-networktool.svg"
+
 function list_network(){
   nmcli device wifi rescan; nmcli -t -f name connection | sed 's/^/network:/g'
 }
 
-function exec_command_with_chosen_element(){
-  network="$(echo $1 | xargs)"
+function run_network(){
+  network="$1"
   if [ "$(nmcli connection | grep "$network")" != "" ]; then
-    nmcli device wifi connect "$network" && notify "normal" "connected to $network"
+    nmcli device wifi rescan
+    nmcli device wifi connect "$network" && notify "normal" "connected to $network" || notify "critical" "failed to connect to $network"
   fi
 }
