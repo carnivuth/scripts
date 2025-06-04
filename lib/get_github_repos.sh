67c7a5be53e85879b59e11447c1f39c44c5884b8
Matteo Longhi <mlonghi@labs.it>
2025-06-04 10:43:23 +0200

    removed bug on file download

--- lib/get_github_repos.sh
+++ lib/get_github_repos.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 source "$HOME/.config/scripts/settings.sh"
 get_file(){
-  if ping -c 1 "api.github.com" > /dev/null 2>&1; then
     curl "https://api.github.com/users/$GITHUB_REPOVIEWER_ACCOUNT/repos?per_page=100" > "$SCRIPTS_LOCAL_FOLDER/repos.json"
-  fi
 }
 function get_github_repos(){
 
   # check if cache is valid
-  if [[ ! -f "$SCRIPTS_LOCAL_FOLDER/repos.json" ]] || [[ "$(wc -l "$SCRIPTS_LOCAL_FOLDER/repos.json")" == "0" ]]; then get_file; fi
+  if [[ ! -f "$SCRIPTS_LOCAL_FOLDER/repos.json" ]] || [[ "$(wc -l "$SCRIPTS_LOCAL_FOLDER/repos.json")" == "0" ]]; then
+    get_file
+  fi
+
   birth_date="$(stat -c '%W' "$SCRIPTS_LOCAL_FOLDER/repos.json" )"
   today="$(date +%s)"
 
