181e0a7746a839607b6d1569c6fa67e1293ed7af
Matteo Longhi <mlonghi@labs.it>
2025-06-11 17:36:34 +0200

    added support for multiple password stores

--- lib/menus/otp.sh
+++ lib/menus/otp.sh
@@ -2,9 +2,14 @@
 source "$HOME/.config/scripts/settings.sh"
 
 list_otp(){
-  find ~/.password-store/ -name '*.gpg'| sed -e 's|.*.password-store/||g' -e 's|.gpg||g' | sed 's/^/otp:/g'
+  for password_store in $PASSWORD_STORES; do
+    find "$password_store"  -name '*.gpg' | sed -e "s|.*$(basename "$password_store")/||g" -e 's|.gpg||g' | sed "s/^/otp:$(basename "$password_store")>/g"
+  done
 }
 
 run_otp(){
-  pass otp -c "$1"
+  echo "$1" | awk -F'>' '{print $1, $2}' | (
+    read password_store password
+    PASSWORD_STORE_DIR="$(find "$HOME" -type d -name "$password_store" )" pass otp -c "$password"
+  )
 }
