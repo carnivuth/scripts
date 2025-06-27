5b8c063d4b238387672f3a877d24b3c9c5429109
Matteo Longhi <mlonghi@labs.it>
2025-06-27 09:44:57 +0200

    added function to download from multiple links

--- bin/yt-downloader.sh
+++ bin/yt-downloader.sh
@@ -7,12 +7,15 @@ FLAGS[c]='SUBS=--embed-subs'
 FLAGS[u]='URL=${OPTARG}'
 FLAGS[f]='FORMAT="-f "${OPTARG}'
 FLAGS[d]='DEST_FOLDER=${OPTARG}'
+FLAGS[l]='LINKS_FILE=${OPTARG}'
 FLAGS[D]='DEBUG=1'
-FLAGS_STRING='acf:u:d:D'
+FLAGS_STRING='acf:u:d:l:D'
 
 declare -A COMMANDS
 COMMANDS[dwl]="download content"
+COMMANDS[nv_album]="download multiple contents"
 YT_DLP_PROGRESS_FILE='/tmp/progress.txt'
+NV_ALBUM_DIR="/tmp/nv_album"
 
 function dwl() {
   if [[ -z $DEST_FOLDER ]] || [[ -z $URL ]]; then help; exit 1; fi
@@ -37,4 +40,13 @@ function dwl() {
   # remove progress file
   rm -f "$YT_DLP_PROGRESS_FILE"
 }
+function nv_album(){
+
+  if [[ -z $LINKS_FILE ]]; then help; exit 1; fi
+  cat "$LINKS_FILE" | while read link; do
+    echo "downloading $link"
+    ($0 dwl -a -d $NV_ALBUM_DIR -u "$link")
+  done
+}
+
 source "$SCRIPTS_LIB_FOLDER/cli.sh"
