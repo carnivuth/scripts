c367dd3f000211d7d54e21fdfb11a57b2ece63a1
Matteo Longhi <mlonghi@labs.it>
2025-06-27 17:04:29 +0200

    fixed hyprpaper config

--- lib/menus/wallpaper.sh
+++ lib/menus/wallpaper.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/bash
 source "$HOME/.config/scripts/settings.sh"
 
-#hyprpaper change background script
-hyprpaper_switcher(){
-  hyprctl hyprpaper preload "$1"
-  hyprctl hyprpaper wallpaper eDP-1,"$1"
-  hyprctl hyprpaper wallpaper HDMI-A-1,"$1"
-  cat <<EOT > "$HOME/.config/hypr/hyprpaper.conf"
-preload = $1
-wallpaper = ,$1
-splash = false
-EOT
-}
-
 list_wallpaper(){
   find $THEMESWITCHER_FOLDERS -name '*.png' -or -name '*.jpg' -or -name '*.jpeg' | sed 's/^/wallpaper:/g'
 }
@@ -21,7 +9,7 @@ run_wallpaper(){
   # run specific scripts based on current desktop
   case "$XDG_CURRENT_DESKTOP" in
     "Hyprland")
-      hyprpaper_switcher "$1"
+      hyprctl hyprpaper reload ,"$1"
       killall -SIGUSR2 waybar
       ;;
     "sway")
