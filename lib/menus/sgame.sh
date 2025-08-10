8b5ad9702cb550e12b0ca5db4c405e97f1686547
carnivuth <matti200042@gmail.com>
2025-08-10 17:21:09 +0200

    changed type from steam to sgame for better searching inside menu

--- lib/menus/sgame.sh
+++ lib/menus/sgame.sh
@@ -3,11 +3,11 @@ source "$HOME/.config/scripts/settings.sh"
 
 STEAM_APPS_FOLDER="$HOME/.steam/root/steamapps"
 
-list_steam(){
-  grep 'name' $STEAM_APPS_FOLDER/appmanifest_*  | awk -F'"' '{print $4}' | sed 's/^/steam:/g'
+list_sgame(){
+  grep 'name' $STEAM_APPS_FOLDER/appmanifest_*  | awk -F'"' '{print $4}' | sed 's/^/sgame:/g'
 }
 
-run_steam(){
+run_sgame(){
   gameid="$(grep -l "$1" "$STEAM_APPS_FOLDER"/appmanifest_* | awk -F'_' '{print $2}' |awk -F '.' '{print $1}')"
   steam -silent "steam://rungameid/$gameid" >> "$SCRIPTS_LOGS_FOLDER/steamrunner.log" 2>&1
 }
