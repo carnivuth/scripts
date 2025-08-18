9a42b2e5045460d220e2e016f728fc557dced519
carnivuth <matti200042@gmail.com>
2025-08-18 09:19:15 +0200

    removed debian support

--- bin/store.sh
+++ bin/store.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 source "$HOME/.config/scripts/settings.sh"
-if [[ -f /etc/os-release ]];then source /etc/os-release; fi
 
 declare -A FLAGS
 FLAGS_STRING='u'
@@ -15,36 +14,21 @@ if [[ "$(whoami)" != 'root' ]];then
 fi
 
 install(){
-  case $ID in
-    "arch")
-      # check for aur flag and use yay to run command
-      if [[ "$AUR" == "TRUE" ]] && [[ -x "$( which yay )" ]]; then
-        yay -Slq | fzf  --preview "yay -Si {1}"  | xargs -ro yay -S
-      else
-        pacman -Slq | fzf  --preview "pacman -Si {1}"  | xargs -ro $SUDO pacman -S
-      fi
-      ;;
-    "debian"|"ubuntu")
-      # acky way to do the same as arch but with apt
-      apt-cache -n search '.*' | awk -F ' - ' '{print $1}' | fzf  --preview "apt-cache show  {1}"  | xargs -ro $SUDO apt-get install
-      ;;
-  esac
+  # check for aur flag and use yay to run command
+  if [[ "$AUR" == "TRUE" ]] && [[ -x "$( which yay )" ]]; then
+    yay -Slq | fzf  --preview "yay -Si {1}"  | xargs -ro yay -S
+  else
+    pacman -Slq | fzf  --preview "pacman -Si {1}"  | xargs -ro $SUDO pacman -S
+  fi
 }
 
 remove(){
-  case $ID in
-    "arch")
-      # check for aur flag and use yay to run command
-      if [[ "$AUR" == "TRUE" ]] && [[ -x "$( which yay )" ]]; then
-        yay -Qq | fzf  --preview "yay -Qi {1}"  | xargs -ro yay -Rns
-      else
-        pacman -Qq | fzf  --preview "pacman -Qi {1}"  | xargs -ro $SUDO pacman -Rns
-      fi
-      ;;
-    "debian"|"ubuntu")
-      dpkg -l | grep '^ii' | awk -F ' ' '{print $2}' | fzf  --preview "apt-cache show  {1}"  | xargs -ro $SUDO apt-get remove
-      ;;
-  esac
+  # check for aur flag and use yay to run command
+  if [[ "$AUR" == "TRUE" ]] && [[ -x "$( which yay )" ]]; then
+    yay -Qq | fzf  --preview "yay -Qi {1}"  | xargs -ro yay -Rns
+  else
+    pacman -Qq | fzf  --preview "pacman -Qi {1}"  | xargs -ro $SUDO pacman -Rns
+  fi
 }
 
 source "$SCRIPTS_LIB_FOLDER/cli.sh"
