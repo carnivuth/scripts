f8fad454b4bd7cdbf6ba945f412f619ed387bc87
Matteo Longhi <mlonghi@labs.it>
2025-09-29 12:06:38 +0200

    added path to collection variable

--- bin/collection_manage.sh
+++ bin/collection_manage.sh
@@ -4,6 +4,9 @@ source "$HOME/.config/scripts/settings.sh"
 # device to read for cd ripping
 DEVICE='/dev/sr0'
 
+# path to beets collection directory
+COLLECTION_DIR="$HOME/collection"
+
 # download only audio files default yes
 AUDIO_ONLY="-x"
 
@@ -16,13 +19,15 @@ declare -A FLAGS
 FLAGS[a]='AUDIO_ONLY=""'
 FLAGS[u]='URL=${OPTARG}'
 FLAGS[d]='DEVICE=${OPTARG}'
+FLAGS[C]='COLLECTION_DIR=${OPTARG}'
 
 declare -A FLAGS_DESCRIPTIONS
 FLAGS_DESCRIPTIONS[a]='disable audio only downloads'
 FLAGS_DESCRIPTIONS[u]='url of the playlist to download'
 FLAGS_DESCRIPTIONS[d]="device to read when ripping cds, default $DEVICE"
+FLAGS_DESCRIPTIONS[C]="Beets collection directory, default $COLLECTION_DIR"
 
-FLAGS_STRING='au:d:'
+FLAGS_STRING='au:C:d:'
 
 declare -A COMMANDS
 COMMANDS[download]="download album from youtube playlist link and import inside beet collection"
@@ -32,6 +37,8 @@ function download() {
 
   # check on url parameter
   if [[ -z $URL ]]; then echo "URL variable not set, use -u option"; help; exit 1; fi
+  if [[ -z $COLLECTION_DIR ]]; then echo "COLLECTION_DIR variable not set, use -C option"; help; exit 1; fi
+  if [[ ! -e "$COLLECTION_DIR/library.db" ]];then  echo "beet database $COLLECTION_DIR/library.db does not exists, maybe collection folder is not mounted"; exit 1; fi
 
   YT_DLP_CMD="yt-dlp \
     --ignore-errors \
@@ -47,20 +54,23 @@ function download() {
 
   $YT_DLP_CMD
 
-  echo "beet -v import $NEW_ALBUM_PATH"
-  beet -v import "$NEW_ALBUM_PATH"
+  echo "beet -d $COLLECTION_DIR -v import $NEW_ALBUM_PATH"
+  beet -d $COLLECTION_DIR -v import "$NEW_ALBUM_PATH"
 }
 
 function ripcd(){
   if [[ -z $DEVICE ]]; then echo "DEVICE variable not set, use -d option"; help; exit 1; fi
+  if [[ -z $COLLECTION_DIR ]]; then echo "COLLECTION_DIR variable not set, use -C option"; help; exit 1; fi
+  if [[ ! -e "$COLLECTION_DIR/library.db" ]];then  echo "beet database $COLLECTION_DIR/library.db does not exists, maybe collection folder is not mounted"; exit 1; fi
+
   (
     cd "$NEW_ALBUM_PATH"
     echo "cdda2wav -vall cddb=-1 speed=4 -B -D $DEVICE"
     cdda2wav -vall cddb=-1 speed=4 -B -D "$DEVICE"
   )
 
-  echo "beet -v import $NEW_ALBUM_PATH"
-  beet -v import "$NEW_ALBUM_PATH"
+  echo "beet -d $COLLECTION_DIR -v import $NEW_ALBUM_PATH"
+  beet -d $COLLECTION_DIR -v import "$NEW_ALBUM_PATH"
 }
 
 source "$SCRIPTS_LIB_FOLDER/cli.sh"
