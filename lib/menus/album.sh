dda36b3071b0425a0a12717bae831984fac3a860
carnivuth <matti200042@gmail.com>
2025-11-09 17:01:13 +0100

    added menus to interact with subsonic server and play tracks with mpv

--- lib/menus/album.sh
+++ lib/menus/album.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/bash
 source "$HOME/.config/scripts/settings.sh"
+source "$SCRIPTS_LIB_FOLDER/control_mpv.sh"
 
 list_album(){
-  curl -L -u $SUBSONIC_USERNAME:$SUBSONIC_PASSWORD "https://$SUBSONIC_API_ENDPOINT/getAlbumList?type=random&v=$SUBSONIC_CLIENT_VERSION&c=$SUBSONIC_CLIENT_NAME&f=json" | jq -r '."subsonic-response".albumList.album[] | "\( .id)|\(.title)"' |  sed 's/^/album:/g'
+  curl -L -u $SUBSONIC_USERNAME:$SUBSONIC_PASSWORD "https://$SUBSONIC_API_ENDPOINT/getAlbumList?size=500&type=random&v=$SUBSONIC_CLIENT_VERSION&c=$SUBSONIC_CLIENT_NAME&f=json" | jq -r '."subsonic-response".albumList.album[] | "\( .id)|\(.title)"' |  sed 's/^/album:/g'
 }
 
 run_album(){
   echo "$1" | awk -F'|' '{print $1, $2}' | (
     read id title
-    killall mpv
     # get tracks in album
     curl -L -u $SUBSONIC_USERNAME:$SUBSONIC_PASSWORD "https://$SUBSONIC_API_ENDPOINT/getMusicDirectory?id=$id&v=$SUBSONIC_CLIENT_VERSION&c=$SUBSONIC_CLIENT_NAME&f=json" | jq -r '."subsonic-response".directory.child[].id' | while read track_id;do
     # play tracks
-      mpv --vid=no "https://$SUBSONIC_USERNAME:$SUBSONIC_PASSWORD@$SUBSONIC_API_ENDPOINT/stream?id=$track_id&v=$SUBSONIC_CLIENT_VERSION&c=$SUBSONIC_CLIENT_NAME"
+
+    append_track_on_mpv "https://$SUBSONIC_USERNAME:$SUBSONIC_PASSWORD@$SUBSONIC_API_ENDPOINT/stream?id=$track_id&v=$SUBSONIC_CLIENT_VERSION&c=$SUBSONIC_CLIENT_NAME"
     done
   )
 }
--- lib/menus/track.sh
+++ lib/menus/track.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/bash
 source "$HOME/.config/scripts/settings.sh"
+source "$SCRIPTS_LIB_FOLDER/control_mpv.sh"
 
 list_track(){
-  curl -L -u $SUBSONIC_USERNAME:$SUBSONIC_PASSWORD "https://$SUBSONIC_API_ENDPOINT/getRandomSongs?size=100&v=$SUBSONIC_CLIENT_VERSION&c=$SUBSONIC_CLIENT_NAME&f=json" | jq -r '."subsonic-response".randomSongs.song[] | "\( .id)|\(.title)"' | sed 's/^/track:/g'
+  curl -L -u $SUBSONIC_USERNAME:$SUBSONIC_PASSWORD \
+    "https://$SUBSONIC_API_ENDPOINT/getRandomSongs?size=500&v=$SUBSONIC_CLIENT_VERSION&c=$SUBSONIC_CLIENT_NAME&f=json" | jq -r '."subsonic-response".randomSongs.song[] | "\( .id)|\(.title)"' | sed 's/^/track:/g'
 }
 
 run_track(){
   echo "$1" | awk -F'|' '{print $1, $2}' | (
     read id title
-    mpv --vid=no "https://$SUBSONIC_USERNAME:$SUBSONIC_PASSWORD@$SUBSONIC_API_ENDPOINT/stream?id=$id&v=$SUBSONIC_CLIENT_VERSION&c=$SUBSONIC_CLIENT_NAME"
+
+    replace_track_on_mpv "https://$SUBSONIC_USERNAME:$SUBSONIC_PASSWORD@$SUBSONIC_API_ENDPOINT/stream?id=$id&v=$SUBSONIC_CLIENT_VERSION&c=$SUBSONIC_CLIENT_NAME"
   )
 }
