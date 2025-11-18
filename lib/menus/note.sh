fed657e91d04c7d91b1abe0d59bcf3c535bc2d21
carnivuth <matti200042@gmail.com>
2025-11-18 11:28:56 +0100

    added notes menu

---
+++ lib/menus/note.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+source "$HOME/.config/scripts/settings.sh"
+source "$SCRIPTS_LIB_FOLDER/launch_webapp.sh"
+NOTES_SITE="https://www.carnivuth.org"
+
+list_note(){
+  curl -L "$NOTES_SITE/index.json"  | jq  '.[] | "\( .title)|\(.permalink)"' -r | sed 's/^/note:/'
+}
+
+run_note(){
+
+  permalink=$(echo $1 | awk -F '|' '{print $2}');
+
+  launch_webapp "$NOTES_SITE/$permalink"
+  # draw attention to the firefox window if running on hyprland
+  if [[ "$XDG_CURRENT_DESKTOP" == 'Hyprland' ]]; then
+    hyprctl dispatch 'focuswindow firefox'
+  fi
+
+}
