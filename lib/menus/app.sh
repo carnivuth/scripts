158e84260c15232003482d6c0a321cbc25cbcbbf
carnivuth <matti200042@gmail.com>
2026-01-19 17:46:03 +0100

    minor adjustments and  improvement to avoid duplicates

--- lib/menus/app.sh
+++ lib/menus/app.sh
@@ -2,10 +2,10 @@
 source "$HOME/.config/scripts/settings.sh"
 
 list_app(){
-  find $HOME/.local/share/applications/ /usr/share/applications/ -name '*.desktop'   -exec basename {} .desktop \; |  sed 's/^/app:/g'
+  find $HOME/.local/share/applications/ /usr/share/applications/ -name '*.desktop'   -exec basename {} .desktop \; | sort -u |  sed 's/^/app:/g'
 }
 
 run_app(){
-  app_file="$(find "$HOME/.local/share/applications/" "/usr/share/applications/" -name "$1.desktop")"
+  app_file="$(find "$HOME/.local/share/applications/" "/usr/share/applications/" -name "$1.desktop" | head -n 1)"
   gio launch "$app_file"
 }
