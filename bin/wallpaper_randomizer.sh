8153a8c72fe4fab504b2feb4659a63651a975f1a
carnivuth <matti200042@gmail.com>
2026-04-30 09:02:12 +0200

    removed broken command

--- bin/wallpaper_randomizer.sh
+++ bin/wallpaper_randomizer.sh
@@ -9,9 +9,7 @@ declare -A COMMANDS
 COMMANDS[change_wall]="change wallpaper randomly"
 
 function change_wall(){
-
-  current_wall=$(hyprctl hyprpaper listloaded)
-  wallpaper=$(find $THEMESWITCHER_FOLDERS -type f ! -name "$(basename "$current_wall")" | shuf -n 1)
+  wallpaper=$(find $THEMESWITCHER_FOLDERS -type f | shuf -n 1)
   hyprctl hyprpaper wallpaper ,"$wallpaper"
 }
 
