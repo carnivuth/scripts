aa23d6347df581d3664850c214de1a40cf1ffca5
carnivuth <matti200042@gmail.com>
2026-05-17 15:17:11 +0200

    fixed some wrong bindings

--- etc/hypr/bindings.lua
+++ etc/hypr/bindings.lua
@@ -20,8 +20,8 @@ hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("pamixer -d 5"))
 
 -- Media player controls
 hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"))
--- hl.bind("SUPER + bracketleft", hl.dsp.exec_cmd("playerctl previous"))
--- hl.bind("SUPER + bracketright,", hl.dsp.exec_cmd("playerctl next"))
+hl.bind("SUPER + bracketleft", hl.dsp.exec_cmd("playerctl previous"))
+hl.bind("SUPER + bracketright", hl.dsp.exec_cmd("playerctl next"))
 
 -- brightness controls
 hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl -d intel_blacklight s +10%"))
@@ -92,20 +92,20 @@ hl.bind("SUPER + K", hl.dsp.focus({direction = "u"}))
 hl.bind("SUPER + J", hl.dsp.focus({direction = "d"}))
 
 -- Move focused window
-hl.bind("SUPER + H", hl.dsp.window.move({ direction = "l"}))
-hl.bind("SUPER + L", hl.dsp.window.move({ direction = "r"}))
-hl.bind("SUPER + K", hl.dsp.window.move({ direction = "u"}))
-hl.bind("SUPER + J", hl.dsp.window.move({ direction = "d"}))
+hl.bind("SUPER + SHIFT + H", hl.dsp.window.move({ direction = "l"}))
+hl.bind("SUPER + SHIFT + L", hl.dsp.window.move({ direction = "r"}))
+hl.bind("SUPER + SHIFT + K", hl.dsp.window.move({ direction = "u"}))
+hl.bind("SUPER + SHIFT + J", hl.dsp.window.move({ direction = "d"}))
 
 -- resize windows
--- hl.bind("SUPER + L", hl.dsp.window.resize({step,0}))
--- hl.bind("SUPER + H", hl.dsp.window.resize({-step,0}))
--- hl.bind("SUPER + K", hl.dsp.window.resize({0,-step}))
--- hl.bind("SUPER + J", hl.dsp.window.resize({0,step}))
+hl.bind("SUPER + CTRL + L", hl.dsp.window.resize({x = step, y = 0, relative = true}))
+hl.bind("SUPER + CTRL + H", hl.dsp.window.resize({x =-step, y = 0, relative = true}))
+hl.bind("SUPER + CTRL + K", hl.dsp.window.resize({x = 0, y = -step, relative = true}))
+hl.bind("SUPER + CTRL + J", hl.dsp.window.resize({x = 0, y= step, relative = true}))
 
 -- Move/resize windows with SUPER + + LMB/RMB and dragging
-hl.bind("ALT + mouse:272", hl.dsp.window.drag(), { mouse = true })
-hl.bind("ALT + mouse:273", hl.dsp.window.resize(), { mouse = true })
+hl.bind("SUPER + mouse:272", hl.dsp.window.drag(), { mouse = true })
+hl.bind("SUPER + mouse:273", hl.dsp.window.resize(), { mouse = true })
 
 -- Switch workspaces with SUPER + + [0-9]
 hl.bind("SUPER + 1", hl.dsp.focus({workspace = "1"}))
@@ -120,13 +120,13 @@ hl.bind("SUPER + 9", hl.dsp.focus({workspace = "9"}))
 hl.bind("SUPER + 0", hl.dsp.focus({workspace = "10"}))
 
 -- Move active window to a workspace with SUPER + + SHIFT + [0-9]
-hl.bind("SUPER + 1", hl.dsp.window.move({ workspace = "1", follow = true}))
-hl.bind("SUPER + 2", hl.dsp.window.move({ workspace = "2", follow = true}))
-hl.bind("SUPER + 3", hl.dsp.window.move({ workspace = "3", follow = true}))
-hl.bind("SUPER + 4", hl.dsp.window.move({ workspace = "4", follow = true}))
-hl.bind("SUPER + 5", hl.dsp.window.move({ workspace = "5", follow = true}))
-hl.bind("SUPER + 6", hl.dsp.window.move({ workspace = "6", follow = true}))
-hl.bind("SUPER + 7", hl.dsp.window.move({ workspace = "7", follow = true}))
-hl.bind("SUPER + 8", hl.dsp.window.move({ workspace = "8", follow = true}))
-hl.bind("SUPER + 9", hl.dsp.window.move({ workspace = "9", follow = true}))
-hl.bind("SUPER + 0", hl.dsp.window.move({ workspace = "10", follow = true}))
+hl.bind("SUPER + SHIFT + 1", hl.dsp.window.move({ workspace = "1", follow = true}))
+hl.bind("SUPER + SHIFT + 2", hl.dsp.window.move({ workspace = "2", follow = true}))
+hl.bind("SUPER + SHIFT + 3", hl.dsp.window.move({ workspace = "3", follow = true}))
+hl.bind("SUPER + SHIFT + 4", hl.dsp.window.move({ workspace = "4", follow = true}))
+hl.bind("SUPER + SHIFT + 5", hl.dsp.window.move({ workspace = "5", follow = true}))
+hl.bind("SUPER + SHIFT + 6", hl.dsp.window.move({ workspace = "6", follow = true}))
+hl.bind("SUPER + SHIFT + 7", hl.dsp.window.move({ workspace = "7", follow = true}))
+hl.bind("SUPER + SHIFT + 8", hl.dsp.window.move({ workspace = "8", follow = true}))
+hl.bind("SUPER + SHIFT + 9", hl.dsp.window.move({ workspace = "9", follow = true}))
+hl.bind("SUPER + SHIFT + 0", hl.dsp.window.move({ workspace = "10", follow = true}))
